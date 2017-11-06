753689c6a7815e43a24a2657fdbf6f3cd9f93345
Mirza Ghulam Rasyid <ghulamcyber@hotmail.com>
2017-11-06 13:09:32 +0700

    Add project files.

---
+++ ThumbnailSharp.Cli/Runner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThumbnailSharp.Cli
+{
+    class Runner
+    {
+
+        /*  
+         *  
+         *  thumbnail.exe <source-image-location> <target-thumbnail-location> <thumbnail-size> <format>
+         *  format: [Jpeg|Bmp|Png|Gif|Icon|Tiff|Exif|Wmf|Emf]
+         *  
+         */
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
