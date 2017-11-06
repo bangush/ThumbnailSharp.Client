21deee80b568f8cb07bb211c3833ab3a98b0af25
Mirza Ghulam Rasyid <ghulamcyber@hotmail.com>
2017-11-06 13:11:24 +0700

    Add entry file

--- ThumbnailSharp.Cli/Runner.cs
+++ ThumbnailSharp.Cli/Runner.cs
@@ -9,13 +9,12 @@ namespace ThumbnailSharp.Cli
     class Runner
     {
 
-        /*  
+        /* 
          *  
          *  thumbnail.exe <source-image-location> <target-thumbnail-location> <thumbnail-size> <format>
          *  format: [Jpeg|Bmp|Png|Gif|Icon|Tiff|Exif|Wmf|Emf]
          *  
          */
-
         static void Main(string[] args)
         {
         }
